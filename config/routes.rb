2e46bc862b50efe6092152f6ded1cde6c5c042a2
Yetrina Battad <hello@yetti.io>
2022-02-15 16:06:44 +1100

    feat: adds carts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'store#index', as: 'store_index'
+  resources :line_items
+  resources :carts
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
